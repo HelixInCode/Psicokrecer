ece3bc9d772be7c1fa30404f7aa40371726939a8
Alejandra <marietsy.911.mm@gmail.com>
2020-09-19 20:21:08 -0300

    script typed

---
+++ src/js/tipeo.js
@@ -0,0 +1,20 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '',
+        '<p>El 90% del éxito se basa simplemente en insistir.</p><p>Woody Allen.</p>',
+        '<p>Las ideas creativas  son como un  capullo delicado, hay que mimarlas para que florezcan.</p><p>Daniel Coleman.</p>',
+        '<p>Todo es un proceso de darse cuenta.</p> <p> Fristz Perls<p>'    
+    ],
+    //stringsElement: '#cadenas-texto',
+    typeSpeed: 70,
+    startDelay:400,
+    backSpeed:25,
+    //smartBackspace:true,
+    shuffle:false,
+    backDelay:3500,
+    loop:true,
+    loopCount: false,
+    showCursorL:true,
+    cursorChar: '|',
+    contentType:'html',
+});
