567e289838b61f00721d31f37dd0662f1390fd74
Alejandra <marietsy.911.mm@gmail.com>
2020-09-28 19:35:50 -0300

    agregado menu de responsive

---
+++ src/js/hamburger.js
@@ -0,0 +1,27 @@
+const $menuItems = document.getElementsByClassName('enlaces')[0];
+const $menuOverlay = document.getElementsByClassName('menu-overlay')[0];
+const $hamburger = document.getElementById('hamburger');
+
+/* $hamburger.addEventListener('click', ()=>{
+    console.log('resolucion: ' + screen.width);
+  if($menuItems.classList.contains('hide')){
+
+    $menuItems.classList.remove('hide')
+    $menuOverlay.classList.remove('hide')
+    
+  }else if(!$menuItems.classList.contains('hide')){
+    
+    $menuItems.classList.add('hide')
+    $menuOverlay.classList.add('hide')
+  }
+})
+$menuOverlay.addEventListener('click', ()=>{
+  if(!$menuItems.classList.contains('hide')){
+    
+    $menuItems.classList.add('hide')
+    $menuOverlay.classList.add('hide')
+  }
+}) */
+$hamburger.addEventListener('click', ()=>{
+    $menuOverlay.classList.toggle('d-none');
+})
