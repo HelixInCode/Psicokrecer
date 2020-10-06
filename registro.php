e9771222e25dcc1156d5b4e2f59a83bb0f7b2c82
Cin Sidan <cinthiaesidan_94@hotmail.com>
2020-10-06 12:40:08 -0300

    Login y Registro de Usuario

--- registro.php
+++ registro.php
@@ -1,10 +1,7 @@
-<?php
-session_start();
-include ('conexion.php');
-?>
+<?php include('conexion.php');?>
 <!doctype html>
 <html lang="es">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -15,62 +12,70 @@ include ('conexion.php');
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/lgn.css">
-  </head>
-  <body>
-
-  <?php
-
-    if(isset($_POST['Enviar'])) { // comprobamos que se hayan enviado los datos del formulario
-
-        if(isset($_POST['username'])&& !empty($_POST['username']) && 
-        isset($_POST['password']) && !empty($_POST['password'])){ 
-            $usuario= mysqli_real_escape_string($conexion,$_POST['username']);
+</head>
+<body>
+    <?php
+        if (isset($_POST['Enviar'])) 
+        {
+            $nombre = mysqli_real_escape_string($conexion,$_POST['username']);
             $clave = mysqli_real_escape_string($conexion,$_POST['password']);
-            $clave = crypt($clave,"pass");
-
-            // comprobamos que los datos ingresados en el formulario coincidan con los de la BD
-            $sql = mysqli_query($conexion,"SELECT id, correo, clave FROM administrador WHERE correo='$usuario' AND clave='$clave'") or die(mysqli_error($conexion));
-            $resultado=mysqli_num_rows($sql);//cuento el número de coincidencias
-            $row = mysqli_fetch_array($sql);
-            //echo "todavia no entro en el if";
-                
-
-                if($resultado==1) {
-                    $_SESSION['id'] = $row['id']; // creamos la sesion "usuario_id" y le asignamos como valor el campo usuario_id
-                    $_SESSION['nombre'] = $row["nombre"]; // creamos la sesion "usuario_nombre" y le asignamos como valor el campo 
-                    header("Location: index.html");
-                }else {
-                
- ?>
-                <div class="alerta-error">Usuario o contraseña incorrectos</div>                    
-                </h1>
-                <?php
-                }
+            $email = mysqli_real_escape_string($conexion,$_POST['mail']);+
+            $imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
             
-        }else{
-          echo "Falta completar campos";
-        }
-    }
 
-?>
-    
-  <div id="login">
+            $sql = mysqli_query($conexion, "SELECT email FROM userblog WHERE email='".$email."'");
+            $coincidencias=mysqli_num_rows($sql);
+            if ($coincidencias>0) 
+            {?> 
+            <div>El correo de usuario elegido ya existe</div>
+            <?php }
+            
+            else {$clave = crypt($clave, "pass");
+                $reg = mysqli_query($conexion, "INSERT INTO userblog (nombreUser, email, clave, imagen) VALUES ('$nombre','$email','$clave','$imagen')") or die(mysqli_error($conexion));
+                if ($reg) {
+                    ?>
+                    <div>Usuario creado correctamente</div>
+                    <meta http-equiv="Refresh" content="2" url="lgn.php" />
+                <?php }
+            else {
+                ?>
+            
+            <div>Error al guardar los datos</div>
+            <?php
+            }
+        } 
+            
+            }
+    ?>   
+    <div id="login">
         <h3 class="text-center letra pt-5">Psicokrecer</h3>
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
+                        <form id="login-form" class="form" action="" method="POST" enctype="multipart/form-data">
+                            <h3 class="text-center text-info">Registro</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">Usuario:</label><br>
-                                <input type="email" name="username" id="username" class="form-control">
+                                <label for="username" class="text-info">Nombre:</label><br>
+                                <input type="text" name="username" id="username" class="form-control" placeholder="Este nombre será público">
+                            </div>
+                            <div class="form-group">
+                                <label for="mail" class="text-info">Email:</label><br>
+                                <input type="mail" name="mail" id="mail" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="password" class="text-info">Contraseña:</label><br>
                                 <input type="password" name="password" id="password" class="form-control">
                             </div>
+
                             <div class="form-group">
+                            <input name="imagen" type="file" accept="image/*">
+                            </div>
+                            
+                            <div class="form-group">
+
+                        
+
                                 
                                 <input type="submit" name="Enviar" class="btn btn-info btn-md" value="Enviar">
                             </div>
@@ -87,6 +92,5 @@ include ('conexion.php');
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
 </body>
 </html>
