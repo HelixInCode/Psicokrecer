6fa0bd698eec4b61b3c9a6d3f35783d6206178d0
Alejandra <marietsy.911.mm@gmail.com>
2020-10-06 17:53:46 -0300

    agregados todos los disenos

--- src/js/mostrar-diseno.js
+++ src/js/mostrar-diseno.js
@@ -4,16 +4,19 @@ const formularios = document.querySelectorAll('.formularios-item');
 const showSelect = (e) =>{
 
     if(selectDiseno.value == 1){//muestra diseño 1
+        console.log(selectDiseno.value);
         formularios[0].classList.remove('d-none');
         formularios[1].classList.add('d-none');
         formularios[2].classList.add('d-none');
     }
     if(selectDiseno.value == 2){//muestra diseño 2
+        console.log(selectDiseno.value);
         formularios[1].classList.remove('d-none');
         formularios[0].classList.add('d-none');
         formularios[2].classList.add('d-none');
     }
     if(selectDiseno.value == 3){//muestra diseño 3
+        console.log(selectDiseno.value);
         formularios[2].classList.remove('d-none');
         formularios[0].classList.add('d-none');
         formularios[1].classList.add('d-none');
